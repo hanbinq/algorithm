806cc74845fd97f5085e95381d0137a95448b583
liangjing <liangj@tangdou.com>
2018-10-09 17:20:53 +0800

    优化选择排序整理

--- Py/05_selected.py
+++ Py/05_selected.py
@@ -1,50 +1,38 @@
 
-'''
+"""
 选择排序：
-在未排序序列中找到最小(大）元素，存放到排序序列
-的起始位置，然后，再从剩余未排序元素中继续寻找最小(大)
-元素，然后放到已排序序列的末尾。以此类推，直到所有元素
-排序完毕。
-'''
+首先在未排序序列中找到最小(大)元素，存放到排序序列的起始位置，然后，再从剩余
+未排序元素中继续寻找最小(大)元素，然后放到已排序序列的末尾。以此类推，直到所
+有元素均排序完毕。
+
+"""
 
 
-# 将小的数字提前
-def select_sort1(alist):
+def selection_sort(alist):
     n = len(alist)
-    for j in range(n - 1):
-        min_index = j
-        for i in range(j + 1, n):
-            if alist[min_index] > alist[i]:
-                min_index = i
-        alist[j], alist[min_index] = alist[min_index], alist[j]
+    # 需要进行n-1次选择操作
+    for i in range(n-1):
+        # 记录最小的位置
+        min_index = i
+        # 从i+1位置到末尾选择出最小数据
+        for j in range(i+1, n):
+            if alist[j] < alist[min_index]:
+                min_index = j
+        # 如果选择出的数据不在正确位置，进行交换
+        if min_index != i:
+            alist[i], alist[min_index] = alist[min_index], alist[i]
 
 
-li = [99, 22, 64, 55, 11, 35, 89, 1, 2]
-select_sort1(li)
-print(li)
+alist = [54, 226, 93, 17, 77, 31, 44, 55, 20]
+selection_sort(alist)
+print(alist)
 
 
-# 将大的数字坠后
-def select_sort2(alist):
-    n = len(alist)
-    count = 1
-    for j in range(n - 1):
-        max_index = 0
-        for i in range(1, n - j):
-            if alist[max_index] < alist[i]:
-                max_index = i
-        alist[max_index], alist[n - count] = alist[n - count], alist[max_index]
-        count += 1
-
-
-li = [99, 22, 64, 55, 11, 35, 89, 1, 2]
-select_sort2(li)
-print(li)
-
-
-
-
-
+"""
+最优时间复杂度：O(n^2)
+最坏时间复杂度：O(n^2)
+稳定性：不稳定(考虑升序每次选择最大的情况)
+"""
 
 
 
